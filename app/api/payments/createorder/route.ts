8702e1d20aa787fa789beae0cc92e0b84c3e5be0
Dev <suthardev1010@gmail.com>
2024-12-11 14:18:51 +0530

    Type asserted to string

--- app/api/payments/createorder/route.ts
+++ app/api/payments/createorder/route.ts
@@ -11,8 +11,8 @@ const generateOrderSchema = z.object({
 const AMOUNT_LAMPORT = 100;
 
 const razorpay = new Razporpay({
-  key_id: process.env.RAZORPAY_KEY_ID!,
-  key_secret: process.env.RAZORPAY_KEY_SECRET!,
+  key_id: process.env.RAZORPAY_KEY_ID as string,
+  key_secret: process.env.RAZORPAY_KEY_SECRET as string,
 });
 
 export async function POST(req: NextRequest) {
